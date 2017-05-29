48682a49c2684c15826260df25e913d3ef23557e
Jaya Kasa <kasa288@gmail.com>
2017-05-29 12:46:17 -0400

    Defined a two helper functions to compute score

--- src/main/scala/GameScore.scala
+++ src/main/scala/GameScore.scala
@@ -1,3 +1,5 @@
+import scala.annotation.tailrec
+
 /**
   * Created by kasa2 on 5/29/2017.
   */
@@ -9,8 +11,37 @@ object GameScore{
 
 class GameScore {
 
+  /**
+    * Method to compute the score for an input String
+    *
+    * @param input
+    * @return
+    */
   def score(input: String): Int = {
-    0
+    // will need a helper function to look up points for symbols
+    def valueReference(in: Char): Int = in match{
+      case 'X' => 10
+      case '/' => 10
+      case '-' => 0
+      // case x => x.toInt - 48
+      case x => x.asDigit
+    }
+
+    /**
+      * Helper Method to calculate the score.
+      * Takes advantage of tailrecursion to compute score
+      *
+      * @param input
+      * @param score
+      * @return
+      */
+    @tailrec
+    def helper(input: List[Char], score: Int): Int = input match {
+      case Nil => score
+      case _ => helper(Nil, score + 1)
+    }
+
+    helper(input.toList, 0)
   }
 
 }
